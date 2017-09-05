d2e23fd292b96e3b45168e5440058bbc6f452411
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-04 19:23:27 -0700

    Footer component imported

---
+++ src/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <footer class="py-5 bg-light">
+            <div class="container">
+                <p class="m-0 text-center">Copyright &copy; <a href="http://kateyeeum.com">Kate Lee</a> 2017</p>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+
+    a{
+        color: #000;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Header from './Header.vue'
 import Main from './Main.vue'
+import Footer from './Footer.vue'
 
 new Vue({
     el:'#app',
     components:{
         'kt-header' : Header,
-        'kt-main' : Main
+        'kt-main' : Main,
+        'kt-footer' : Footer
     }
 })
