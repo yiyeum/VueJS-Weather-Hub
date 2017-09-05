bf9efe9f219b2b610daa6599eb4902d216ceb31b
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-05 02:58:10 -0700

    router-link replacing a tag

--- src/Header.vue
+++ src/Header.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="hubHeader text-center">
         <p class="hubSmallTitle">Welcome to</p>
-        <p class="hubTitle">Weather Hub</p>
+        <p class="hubTitle"><router-link to="/">Weather Hub</router-link></p>
 
         <nav class="navbar navbar-expand-lg">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Local Weather</a>
+                    <router-link to="/">Vancouver Now</router-link>
                 </li>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Search Weather</a>
+                    <router-link to="/searchWeather">Search Weather</router-link>
                 </li>
             </ul>
         </nav>
@@ -25,6 +26,7 @@
         padding-top: 3.5%;
         padding-bottom: 3.5%;
         line-height: 0.8;
+        background-color: #e6ffee;
     }
 
     .hubSmallTitle {
@@ -37,7 +39,23 @@
         margin-bottom: 2%;
     }
 
+    .hubTitle a:hover{
+        color: #595959;
+        text-decoration: none;
+    }
+
     ul.navbar-nav{
         margin: 0 auto;
     }
+
+    nav.navbar ul.navbar-nav li.nav-item a{
+        padding: 3px;
+        color: #a6a6a6;
+    }
+
+    nav.navbar ul.navbar-nav li.nav-item a:hover{
+        color: #4d4d4d;
+        text-decoration: none;
+    }
+
 </style>
