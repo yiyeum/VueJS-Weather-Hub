be5e5faa5942e2bf360e7c8c9311e691ff8b5d0a
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-04 19:18:15 -0700

    Header created

---
+++ src/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hubHeader text-center">
+        <p class="hubSmallTitle">Welcome to</p>
+        <p class="hubTitle">Weather Hub</p>
+
+        <nav class="navbar navbar-expand-lg">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Local Weather</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Search Weather</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+    .hubHeader {
+        margin-top: 5%;
+        line-height: 0.8;
+    }
+
+    .hubSmallTitle {
+        font-size: 20px;
+    }
+
+    .hubTitle {
+        font-size: 50px;
+        letter-spacing: 9px;
+    }
+
+    ul.navbar-nav{
+        margin: 0 auto;
+    }
+</style>
