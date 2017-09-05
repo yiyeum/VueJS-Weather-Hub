93d8b91fe8a072e8b91a5a599a1f5992374702bd
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-04 19:19:48 -0700

    Main Component imported

---
+++ src/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p></p>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+import Header from './Header.vue'
+import Main from './Main.vue'
 
 new Vue({
+    el:'#app',
+    components:{
+        'kt-header' : Header,
+        'kt-main' : Main
+    }
 })
