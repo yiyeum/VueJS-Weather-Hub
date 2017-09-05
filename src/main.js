f2648daae9524b5fdf9832dd2429cd973d79f9dd
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-05 02:20:36 -0700

    Router setting

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import Header from './Header.vue'
 import Main from './Main.vue'
 import Footer from './Footer.vue'
+import Router from 'vue-router'
+import {routes} from './router.js'
+
+Vue.use(Router);
+const router = new Router({
+    routes
+});
 
 new Vue({
     el:'#app',
+    router,
     components:{
         'kt-header' : Header,
         'kt-main' : Main,
---
+++ src/router.js
@@ -0,0 +1,10 @@
+/**
+ * Created by KateYeEumLee on 2017-09-05.
+ */
+import Main from './Main.vue'
+import Search from './Search.vue'
+
+export const routes =[
+    {path: '/', component : Main},
+    {path: '/searchWeather',component : Search}
+]
